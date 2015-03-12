93b72505e5e8cdaeacf07323f15bd11dab1bb125
PaulTomchik <PaulTomchik@users.noreply.github.com>
2015-03-11 20:51:19 -0400

    Update InstallSpark.sh

--- InstallSpark.sh
+++ InstallSpark.sh
@@ -2,7 +2,7 @@ cd ~
 mkdir -p ~/opt
 cd opt
 wget http://mirrors.gigenet.com/apache/spark/spark-1.2.1/spark-1.2.1.tgz
-tar zxvf spark-1.2.1-bin-hadoop2.4.tgz
-ln -s ~/opt/spark-1.2.1-bin-hadoop2.4 spark
+tar zxvf spark-1.2.1.tgz
+ln -s ~/opt/spark-1.2.1.tgz spark
 
 echo  'export PATH="${PATH}:${HOME}/opt/spark/bin"' >> ~/.bashrc
