5904c663069ccb62380aa87584a600db7604b5f7
PaulTomchik <PaulTomchik@users.noreply.github.com>
2015-03-11 22:17:51 -0400

    Update ConfigureMaster.sh

--- ConfigureMaster.sh
+++ ConfigureMaster.sh
@@ -1,1 +1,6 @@
 sed '/# - SPARK_MASTER_IP.*$/a export SPARK_MASTER_IP=192.168.56.110' ~/opt/spark/conf/spark-env.sh.template >> ~/opt/spark/conf/spark-env.sh
+
+
+printf "sparkslave1\nsparkslave2" >> ~/opt/spark/conf/slaves
+
+cp mastersshconfig ~/.ssh/config
